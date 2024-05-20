de8e575e9b99f681414ad9c93f38d329fd85d67f
SO YEON LEE <lana_75@naver.comgit>
2024-05-20 14:36:24 +0900

    기초 구성 및 뷰 완성

---
+++ src/main/java/com/example/fruitmarketbuyer3/FruitmarketBuyer3Application.java
@@ -0,0 +1,13 @@
+package com.example.fruitmarketbuyer3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitmarketBuyer3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitmarketBuyer3Application.class, args);
+    }
+
+}
